54d0e29e653cd5f6b9fbb16039ed7dbc26a22c4a
Dario Nahuel Rodriguez <darionahuel.parse@gmail.com>
2019-01-17 23:30:19 -0300

    Fix eslint

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -1,20 +1,20 @@
-'use strict'
+'use strict';
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use('Schema');
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('full_name', 80).notNullable().unique()
-      table.timestamps()
-    })
+    this.create('users', table => {
+      table.increments();
+      table.string('full_name', 80).notNullable().unique();
+      table.timestamps();
+    });
   }
 
   down () {
-    this.drop('users')
+    this.drop('users');
   }
 }
 
-module.exports = UserSchema
+module.exports = UserSchema;
