db19be901bd7bdc5e6c62f2d86eb69161636c793
Nico Acosta <nico.a.acosta@gmail.com>
2019-02-27 18:36:16 -0300

    added date validations to match controller

--- app/Controllers/Http/MatchController.js
+++ app/Controllers/Http/MatchController.js
@@ -1,16 +1,29 @@
 'use strict';
 
-const Match = use('App/Models/Match');
-class MatchController {
+const moment = use('moment');
 
+const Match = use('App/Models/Match');
+
+const startOfToday = moment().startOf('day').format('YYYY-MM-DD HH:mm:ss');
+
+const matchTaken = async timeId => {
+  const matches = await Match.query()
+    .where('created_at', '>', startOfToday)
+    .where('time_id', timeId).fetch();
+  return !!matches.size();
+};
+
+class MatchController {
   async index ({ response }) {
-    response.json(await Match.all());
+    response.json(await Match.query().where('created_at', '>', startOfToday).fetch());
   }
 
   async store ({ request, response }) {
     const { user1_id, user2_id, time_id } = request.all();
 
-    if (user1_id === user2_id) return response.status(400).json('bad request');
+    if (user1_id === user2_id || await matchTaken(time_id)) {
+      return response.status(200).json('bad request');
+    }
 
     const match = await Match.create({ user1_id, user2_id, time_id });
 
--- app/Models/Match.js
+++ app/Models/Match.js
@@ -1,16 +1,15 @@
-'use strict'
+'use strict';
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use('Model');
 
 class Match extends Model {
-
   user () {
-    return this.hasMany('App/Models/User')
+    return this.hasMany('App/Models/User');
   }
 
   time () {
-    return this.hasOne('App/Models/Time')
+    return this.hasOne('App/Models/Time');
   }
 
   static get hidden () {
@@ -18,4 +17,4 @@ class Match extends Model {
   }
 }
 
-module.exports = Match
+module.exports = Match;
